e57e4587c4f0fc9d711329cda7c6948aaf3b9a93
ThomasReed98 <thomas.reed.work@gmail.com>
2025-09-11 08:32:09 -0400

    9/11

--- items/jokers/madcap/139_cont2nuum.lua
+++ items/jokers/madcap/139_cont2nuum.lua
@@ -4,6 +4,17 @@ function Madcap.Funcs.is_scoring_area(area)
     return false
 end
 
+function Madcap.Funcs.do_after_scoring_stuff()
+    if next(SMODS.find_card('j_rgmc_cont2nuum')) then
+        MadLib.loop_func(G.hand.cards, function(v)
+            MadLib.simple_event(function()
+                v.area:remove_from_highlighted(v)
+                return true
+            end, 0.05, 'after')
+        end)
+    end
+end
+
 return {
     categories = {
         'Unfinished Content',
